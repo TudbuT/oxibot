2f35665aa75bcc6fb94abf665f9a3a53d665faae
TudbuT <tudbut@tudbut.de>
2022-09-12 19:36:54 +0200

    add the basic structure to start the bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{Client, prelude::GatewayIntents};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let token = match env::var("TOKEN") {
+        Ok(t) => t,
+        Err(_) => panic!("No token was provided (env[TOKEN])"),
+    };
+    let mut client = Client::builder(token, GatewayIntents::GUILDS).await.expect("unable to start client");
+    if let Err(e) = client.start().await {
+        panic!("Error starting client: {e}")
+    }
 }
