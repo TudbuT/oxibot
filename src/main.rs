e958fa5438c4d61832303de58782b71456e062f3
TudbuT <tudbut@tudbut.de>
2022-09-12 19:47:11 +0200

    more basics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use serenity::{Client, prelude::GatewayIntents};
+use serenity::{Client, prelude::*, framework::{StandardFramework, standard::{macros::command, CommandResult}}, model::prelude::*};
 use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
@@ -7,8 +11,18 @@ async fn main() {
         Ok(t) => t,
         Err(_) => panic!("No token was provided (env[TOKEN])"),
     };
-    let mut client = Client::builder(token, GatewayIntents::GUILDS).await.expect("unable to start client");
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("oxi!"));
+    let mut client = Client::builder(token, GatewayIntents::GUILDS)
+        .event_handler(Handler)
+        .framework(framework)
+        .await.expect("unable to start client");
     if let Err(e) = client.start().await {
         panic!("Error starting client: {e}")
     }
+}
+
+#[command]
+async fn test(ctx: &Context, msg: &Message) -> CommandResult {
+    Ok(())
 }
